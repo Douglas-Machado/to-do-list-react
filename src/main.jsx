d12a399160949f28605138696f6ab479471d8257
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-04-23 15:50:40 -0300

    v1.0
    * add localstorage and created a form component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from './App' //importando o componente app
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
